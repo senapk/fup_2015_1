4245195f0eb2e5bb8f1de0735431425108b827ab
David Sena <sena.ufc@gmail.com>
2015-03-12 16:15:54 -0300

    jogos

--- aulas/aula07_es_jogos/zerim.py
+++ aulas/aula07_es_jogos/zerim.py
@@ -4,9 +4,9 @@ op3 = input()
 
 if op1 == op2 and op2 == op3:
     print "empate"
-elif op1 != op2:
-    print "jog1"
-elif op2 != op3:
-    print "jog2"
-else:
+elif op1 == op2:
     print "jog3"
+elif op2 == op3:
+    print "jog1"
+else:
+    print "jog2"
